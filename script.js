c0238ee0b034496e56e692c0ed9fff0d39d5b8fc
manummimavindya <manummi2023@gmail.com>
2025-11-09 21:15:09 +0530

    updated script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.Manumi Mavindya CV').addEventListener('click', () => {
+    console.log("Downloading CV...");
+});
+
+// const form = document.getElementById('contactForm');
+//
+// document.getElementById('sendBtn').addEventListener('click', () =>{
+//     form.reset();
+// })
+
+
